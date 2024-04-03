500d2cb976e191c9b43c1faaaef0a121a7df4fe0
NANDANI <nandanip.2002@gmail.com>
2024-04-03 22:16:26 +0800

    Added view and edit profile feature.

--- app/__init__.py
+++ app/__init__.py
@@ -41,6 +41,9 @@ def create_app(config_class=Config):
 	from app.errors import bp as errors_bp
 	app.register_blueprint(errors_bp)
 
+	from app.users import bp as users_bp
+	app.register_blueprint(users_bp, url_prefix='/users')
+
 	from app.auth import bp as auth_bp
 	app.register_blueprint(auth_bp, url_prefix='/auth')
 
@@ -48,35 +51,33 @@ def create_app(config_class=Config):
 	if not app.debug:
 
 		# Email logger
-	    if app.config['MAIL_SERVER']:
-	        auth = None
-	        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-	            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-	        secure = None
-	        if app.config['MAIL_USE_TLS']:
-	            secure = ()
-	        mail_handler = SMTPHandler(
-	            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+		if app.config['MAIL_SERVER']:
+			auth = None
+			if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+				auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+			secure = None
+			if app.config['MAIL_USE_TLS']:
+				secure = ()
+			mail_handler = SMTPHandler(
+				mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
 	            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
 	            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-	            credentials=auth, secure=secure)
-	        mail_handler.setLevel(logging.ERROR)
-	        app.logger.addHandler(mail_handler)
+	            credentials=auth, secure=secure
+			)
+			mail_handler.setLevel(logging.ERROR)
+			app.logger.addHandler(mail_handler)
 
-
-	    # File logger
-	    if not os.path.exists('logs'):
-	        os.mkdir('logs')
-	    file_handler = RotatingFileHandler('logs/error.log', maxBytes=10240,
+		# File logger
+		if not os.path.exists('logs'):
+			os.mkdir('logs')
+		file_handler = RotatingFileHandler('logs/error.log', maxBytes=10240,
 	                                       backupCount=10)
-	    file_handler.setFormatter(logging.Formatter(
+		file_handler.setFormatter(logging.Formatter(
 	        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-	    file_handler.setLevel(logging.INFO)
-	    app.logger.addHandler(file_handler)
+		file_handler.setLevel(logging.INFO)
+		app.logger.addHandler(file_handler)
 
-	    app.logger.setLevel(logging.INFO)
-	    app.logger.info('BuySell marketplace')
+		app.logger.setLevel(logging.INFO)
+		app.logger.info('BuySell marketplace')
 
 	return app
-
-
