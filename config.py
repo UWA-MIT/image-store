554aada8657ae904a93aaacc07dea9e7c83b8faa
Konstantin Tagintsev <k.tagintsev@gmail.com>
2024-04-06 21:14:10 +0800

    Buy page where users can see all items on sale

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-os.environ['FLASK_CONFIG'] = ''
+os.environ['FLASK_CONFIG'] = 'test'
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '52e4da2524c9580fd6bcff60fab06189'
