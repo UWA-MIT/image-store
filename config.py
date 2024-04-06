825128fbab660471a5325d5f0c41522ad4aacf38
saicharan0812000 <Saicharannaidu2000@gmail.com>
2024-04-06 22:08:54 +0800

    Removed unused code. Styled the home page. Moved from config.py to .flaskenv

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-os.environ['FLASK_CONFIG'] = ''
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '52e4da2524c9580fd6bcff60fab06189'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
