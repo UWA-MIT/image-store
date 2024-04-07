6cbbd6d46fe7c7bf618fda1949d1aaeb7c3600ff
Konstantin Tagintsev <k.tagintsev@gmail.com>
2024-04-07 14:09:42 +0800

    Buy an image

--- app/products/routes.py
+++ app/products/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template, url_for, request, current_app
+from flask import render_template, url_for, flash, redirect, request, current_app, jsonify
+from datetime import datetime
 import sqlalchemy as sa
+
 from app import db
 from app.models.product import Product
 from app.models.user import User
 from app.products import bp
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 @bp.route('/buy')
@@ -22,3 +24,21 @@ def buy():
     prev_url = url_for('products.buy', page=products.prev_num) \
         if products.has_prev else None
     return render_template('products/buy.html', title='Buy', products=products.items, next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/<product_id>/buy', methods=['GET'])
+@login_required
+def buy_product(product_id):
+    product = db.first_or_404(sa.select(Product).where(Product.id == product_id, Product.is_sold == False))
+
+    if (int(round(product.price)) > current_user.reward_points):
+        return jsonify({"success": False, "message": "Sorry, you do not have enough reward points to buy this product"}), 200
+
+    product.is_sold = True
+    product.buyer_id = current_user.id
+    product.sold_at = datetime.utcnow()
+
+    user = db.session.get(User, current_user.id)
+    user.reward_points = user.reward_points - int(round(product.price))
+    db.session.commit()
+    return jsonify({"success": True, "message": "Congratulations, your product purchase has been completed successfully!"}), 200
---
+++ app/static/js/custom.js
@@ -0,0 +1,30 @@
+function transferDataToModal(elem) {
+    const id = elem.getAttribute('data-id');
+    const name = elem.getAttribute('data-name');
+    const price = elem.getAttribute('data-price');
+    document.getElementById('nameImage').innerText = name;
+    document.getElementById('priceImage').innerText = '$' + price;
+    const confirmButton = document.querySelector('button[onclick^="buyImage"]');
+    confirmButton.setAttribute('onclick', `buyImage('${id}')`);
+}
+function buyImage(id) {
+  var url = id + '/buy';
+  $.ajax({
+    url: url,
+    type: 'GET',
+    success: function(response) {
+      alertInfo(response.message);
+      if(response.success){
+          $('[data-id="' + id + '"].card.area').remove();
+      }
+      $('#buyModal').modal('hide');
+    },
+    error: function(xhr, status, error) {
+      alertInfo(error);
+      $('#buyModal').modal('hide');
+    }
+  });
+}
+function alertInfo(message){
+    $('.base').prepend('<div class="alert alert-info" role="alert">' + message + '</div>');
+}
