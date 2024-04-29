63b9f4b214f87ebb421e35523afd99108ebfc0c1
Md Rayhan Chowdhury <ray@raynux.com>
2024-04-29 12:50:59 +0800

    Added slide show in the home page to display latest generated image

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -47,6 +47,48 @@ $(document).ready(function () {
       }
     });
   });
+
+
+  // Slick carousal
+  $('.latest-images-carousel').slick({
+      dots: true,
+      infinite: false,
+      autoplay: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      // centerMode: true,
+      // centerPadding: '60px',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            centerMode: true,
+            centerPadding: '40px',
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+            centerPadding: '30px',
+          }
+        }
+      ]
+    });
 });
 
 function transferDataToModal(elem) {
